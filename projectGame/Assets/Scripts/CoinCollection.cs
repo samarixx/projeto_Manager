23a2572f093cfa0ac414ea2861e56f5dfda532cf
samara <sam.samcosta07@gmail.com>
2026-06-25 15:46:23 -0300

    teste

---
+++ projectGame/Assets/Scripts/CoinCollection.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CoinCollection : MonoBehaviour
+{
+
+    private int Coin = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Coin")
+        {
+            Coin++
+            Debug.Log(Coin);
+        }
+    }
+}
