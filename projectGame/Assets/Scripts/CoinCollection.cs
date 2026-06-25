354e3bc680b49909a1850999f9eb686e2e0fb571
samara <sam.samcosta07@gmail.com>
2026-06-25 15:50:50 -0300

    aaaaaaa

--- projectGame/Assets/Scripts/CoinCollection.cs
+++ projectGame/Assets/Scripts/CoinCollection.cs
@@ -4,12 +4,15 @@ public class CoinCollection : MonoBehaviour
 {
 
     private int Coin = 0;
+    
+    public TextMeshProGUI coinText;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Coin")
         {
             Coin++
+            coinText.text = "Coin: " + Coin.ToString();    
             Debug.Log(Coin);
         }
     }
